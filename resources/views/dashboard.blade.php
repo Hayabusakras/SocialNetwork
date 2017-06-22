6f08d8d8a958a2c477f3d52374d01616ae4a8269
Hayabusakras <Hayabusakras@gmail.com>
2017-06-22 16:32:35 +0300

    creating Posts(model and controller)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,9 +3,10 @@
     <section class="row new-post">
         <div class="col-md-6 col-md-offset-3">
             <header><h3>What do you have to say?</h3></header>
-            <form action="#">
+            <form action="{{ route('post.create') }}" method="POST">
+                {{csrf_field()}}
                 <div class="form-group">
-                    <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Your Post"></textarea>
+                    <textarea class="form-control" name="body" id="new-post" rows="5" placeholder="Your Post"></textarea>
                 </div>
                 <button type="submit" class="btn btn-primary">Create Post</button>
             </form>
