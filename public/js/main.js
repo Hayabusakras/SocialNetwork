91864dbd0a13ea652d500548ca5a2ad5aae7d136
Hayabusakras <Hayabusakras@gmail.com>
2017-06-23 16:19:12 +0300

    updating DB and View

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,10 @@
 var postId = 0;
+var postBodyElement = null;
 $('.post').find('.interaction').find('.edit').on('click', function(event) {
     event.preventDefault();
-    var postBody = event.target.parentNode.parentNode.childNodes[1].textContent;
+
+    postBodyElement = event.target.parentNode.parentNode.childNodes[1];
+    var postBody = postBodyElement.textContent;
     postId = event.target.parentNode.parentNode.dataset['postid'];
     $('#post-body').val(postBody);
     $('#edit-modal').modal();
@@ -11,9 +14,10 @@ $('#modal-save').on('click', function() {
     $.ajax({
         method: 'POST',
         url: url,
-        data: { body: $('#post-body').val(), postId: postId, _token: token,  }
+        data: { body: $('#post-body').val(), postId: postId, _token: token  }
     })
         .done(function(msg) {
-            console.log(msg['message']);
+            $(postBodyElement).text(msg['new_body']);
+            $('#edit-modal').modal('hide');
         });
 });
